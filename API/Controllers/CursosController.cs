cdd66dfc1c5580120372526fa8fe72cdcd07c259
Detz <deiby.tolentino@unas.edu.pe>
2025-07-07 15:58:33 -0500

    Prueba Archivo

--- API/Controllers/CursosController.cs
+++ API/Controllers/CursosController.cs
@@ -35,6 +35,7 @@ namespace API.Controllers
             return Ok(response);
         }
         
+
         /*
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CursoResponseDto>>> GetCursos()
